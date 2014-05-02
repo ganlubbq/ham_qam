238622bbcaccb80aac61ef59e2aeb8cb597e8e94
Nader Behdin <naderbehdin@Nader-Behdins-MacBook.local>
2014-05-02 12:40:41 -0700

    Added the rest of decoding. not quite there, but very close

--- nadertest2.py
+++ nadertest2.py
@@ -6,9 +6,7 @@ from qam import *
 prefix = np.array([[0],[2],[10],[8]])
 bits= np.array([[1],[1],[1],[1],[1],[0],[2],[10],[8],[6],[11] , [0], [4], [2],[1], [2], [5], [6], [8], [6], [10], [3], [14],[0],[10]])
 randombits = randomBits(16)
-#bits = prefix
-#bits = np.array([0,2,10,8,5,11,0,5,7,13,9])
-#bits = np.concatenate(bits, prefix)
+
 fs = 44100
 baud = 300
 Nbits = len(bits)
@@ -17,41 +15,32 @@ f0=1800
 N = Nbits * Ns
 code = np.array((-2-2j, -2-1j,-2+2j,-2+1j,-1-2j,-1-1j,-1+2j,-1+1j,+2-2j,+2-1j,+2+2j,+2+1j,1-2j,+1-1j,1+2j,1+1j))/2
 
-
+# modulate both the bits and the prefix, so you can maybe correlate
+# with modulated prefix:
 M, t = makeSymbols(bits, fs, code, Ns)
 M_prefix, t_prefix = makeSymbols(prefix, fs, code, Ns)
 
 #This is the fully modulated bit string to be sent:
 QAM = (M.real.ravel()*cos(2*pi*f0*t) - M.imag.ravel()*sin(2*pi*f0*t))/2/sqrt(2)
 
+#Do it to the prefix also:
 QAM_prefix = (M_prefix.real.ravel()*cos(2*pi*f0*t_prefix) - M_prefix.imag.ravel()*sin(2*pi*f0*t_prefix))/2/sqrt(2) 
 
-# fig = figure(figsize = (16,4))
-# plot(t,QAM)
-# plot(t_prefix,QAM_prefix)
-# xlabel('time [s]')
-# title("QAM=16 of the sequence:"+ np.array2string(transpose(bits)))
-# show()
-
+#modulated correlation (of sunusoids):
 corr = np.correlate(QAM,QAM_prefix,"same")
 corr_index = argmax(abs(corr))
-print "index: ", corr_index
+print "index from corr: ", corr_index
 
 # fig = figure()
 # plot(abs(corr))
 # title("modulated sinusoid corr")
 
-
-# fig = figure()
-# plot(M.real.ravel()*cos(2*pi*f0*t))
-#fig = figure()
-#scatter(np.real(M), np.imag(M))
-#show()
-#print code[bits]
-
+# -----------------
 #begin demodulation:
 r,i = demod(QAM, f0, t)
 
+
+#lowpass the signals to remove images
 lp = signal.firwin(100,pi*f0/4,nyq=44100.0/2.0)
 r_lp = signal.fftconvolve(lp,r)[50:]
 i_lp = signal.fftconvolve(lp, i)[50:]
@@ -60,28 +49,12 @@ i_lp = signal.fftconvolve(lp, i)[50:]
 frange = np.linspace(-fs/2,fs/2,len(r))
 frange_filt = np.linspace(-fs/2,fs/2,len(r_lp))
 
-# fig = figure()
-# plot(M.real.ravel())
-# plot(r_lp)
-# show()
 
-
-
-# fig = figure()
-# plot(M_prefix.real.ravel())
-# plot(r_lp.ravel())
-# title("real")
-# fig = figure()
-
-# plot(M_prefix.imag.ravel())
-# plot(i_lp.ravel())
-# title("imaginary")
-# show()
 corr_r = abs(signal.correlate(r_lp.ravel(),M_prefix.real.ravel(),"same"))
 corr_i = abs(signal.correlate(i_lp.ravel(), M_prefix.imag.ravel(),"same"))
 
 corr_index = argmax(corr_r)
-print "index: ", corr_index 
+print "index from corr: ", corr_index 
 
 # fig = figure()
 # plot(corr_r)
@@ -89,11 +62,50 @@ print "index: ", corr_index
 # title("demodulated bits corr")
 # show()
 
+
+#This next block of code is supposed to trim everything that isn't
+#what we want from the front (afte corr), but I don't think it 
+#is 100% working. Close, though, off by a bit or two?
 r = r_lp[corr_index:]
 
 i = i_lp[corr_index:]
 
 fig = figure()
 plot(r)
-plot(M.real.ravel()[Ns*6:])
+#5 is the number of garbage symbols i put in before the prefix in the signal
+plot(M.real.ravel()[Ns*5:])
 show()
+
+#need to know number of bits for this to work (packet size i guess?)
+#Nbits might be wrong here, since it really needs the nubmer of bits
+#minus the amount you chopped off after correlating
+idx = r_[0:len(r):len(r)/Nbits]
+r = r/max(r)*max(M.real.ravel())*2 #change this 2 depending on the input amplitude
+i = i/max(i)*max(M.imag.ravel())*2 #change this 2 depending on the input amplitude
+r = np.around(r)
+i = np.around(i)
+r_dec = np.around(r[idx])
+i_dec = np.around(i[idx])
+offset = len(M.real.ravel())/Nbits
+
+print "len(r)", len(r)
+print "len(r_dec)", len(r_dec)
+
+r_avg = []
+i_avg = []
+periods = r_[0:len(r):offset]
+
+
+for prd in periods[0:-1]:
+    current_avg_r = np.average(r[prd:prd+offset])
+    r_avg = np.append(r_avg, current_avg_r)
+    
+    current_avg_i = np.average(i[prd:prd+offset])
+    i_avg = np.append(i_avg, current_avg_i)
+    
+
+decoded = np.around(r_avg) + 1j*np.around(i_avg) 
+
+finalbits = np.array([])
+decoded = decoded/2
+
