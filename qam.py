5841442f28e2785b6d69b214ff9066c367b0da5e
Vikram Iyer <viyers1@gmail.com>
2014-05-05 15:41:18 -0700

    Added BER function, doesn't quite look right

--- qam.py
+++ qam.py
@@ -1,3 +1,4 @@
+from numpy import random
 from scipy import signal
 import numpy as np
 import pylab as plt
@@ -51,14 +52,14 @@ def hex_to_symbols():
     return
 
 def symbols_to_hex(symbols):
-    #for s in symbols:
-       #print s.real
-       #print s.imag
-       #r = DECODE[int(s.real)][int(s.imag)]
-       #print r.keys()
-       #print (int(s.imag) in r.keys())
-       #print r[int(s.imag)]
-    return [DECODE[int(s.real)][int(s.imag)] for s in symbols]
+    decoded = []
+    for s in symbols:
+        try:
+            decoded.append(DECODE[int(s.real)][int(s.imag)])
+        except KeyError as e:
+            decoded.append(-1)
+
+    return decoded
 
 def mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=True, plot=False):
     Ns = fs/baud
@@ -78,24 +79,23 @@ def mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=True, plo
     M_prefix = np.tile(code[prefix],(1,Ns))
     t = np.r_[0.0:N]/fs
     t_prefix = np.r_[0.0:len(prefix)*Ns]/fs
+    if plot:
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(t_prefix, M_prefix.real.ravel())
+        plt.ylim(-3,3)
+        plt.title("Real Values of Sent Symbols")
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(t_prefix, M_prefix.real.ravel())
-    plt.ylim(-3,3)
-    plt.title("Real Values of Sent Symbols")
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(t_prefix, M_prefix.imag.ravel())
+        plt.ylim(-3,3)
+        plt.title("Imaginary Values of Sent Symbols")
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(t_prefix, M_prefix.imag.ravel())
-    plt.ylim(-3,3)
-    plt.title("Imaginary Values of Sent Symbols")
+        fig = plt.figure()
+        plt.scatter(M.real.ravel(), M.imag.ravel())
 
-
-    fig = plt.figure()
-    plt.scatter(M.real.ravel(), M.imag.ravel())
-
-    plt.xlabel('Real')
-    plt.ylabel('Imag')
-    plt.title("QAM=16 of the sequence:"+ np.array2string(np.transpose(bits)))
+        plt.xlabel('Real')
+        plt.ylabel('Imag')
+        plt.title("QAM=16 of the sequence:"+ np.array2string(np.transpose(bits)))
 
     #fig = figure(figsize = (16,4))
     #plot(t,QAM.real)
@@ -104,6 +104,18 @@ def mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=True, plo
 
     QAM = (M.real.ravel()*np.cos(2*np.pi*f0*t) -
             M.imag.ravel()*np.sin(2*np.pi*f0*t))/2/np.sqrt(2)
+    if shaped:
+        x = np.r_[-2:2:(1.0/147)]
+        h = np.sinc(x)*signal.hann(147*4)
+        impulses = np.zeros(len(bits)*Ns)
+        for i,b in enumerate(bits):
+            impulses[i*Ns]=code[int(b)]
+        QAM = signal.fftconvolve(impulses, h)
+       #print len(QAM)
+        QAM = QAM[(len(QAM)-N)/4:-(len(QAM)-N)*3/4]
+       #print len(t)
+       #print len(QAM)
+        #t1 = r_[0.0:len(QAM)]/fs
 
     return QAM, M, t, M_prefix, t_prefix
 
@@ -118,8 +130,8 @@ def demod_QAM16(QAM, t, f0=1800, fs=48000):
     i_lp = signal.fftconvolve(lp, i)
 
     #fig = figure(figsize = (16,4))
-    frange = np.linspace(-fs/2,fs/2,len(r))
-    frange_filt = np.linspace(-fs/2,fs/2,len(r_lp))
+    #frange = np.linspace(-fs/2,fs/2,len(r))
+    #frange_filt = np.linspace(-fs/2,fs/2,len(r_lp))
     #plt.plot(frange_filt, abs(fft.fftshift(fft.fft(lp))))
     '''
     ylim(-3,3)
@@ -135,21 +147,24 @@ def demod_QAM16(QAM, t, f0=1800, fs=48000):
     i_lp = i_lp[num_taps/2:-num_taps/2+1]
     return r_lp, i_lp
 
-def detect_sync(r, i, sync, sync_bits, fs=48000, baud=300):
+def detect_sync(r, i, sync, sync_bits, fs=48000, baud=300, plot=False):
     Ns = fs/baud
     corr_r = abs(signal.correlate(r.ravel(), sync.real.ravel(),"same"))
     corr_i = abs(signal.correlate(i.ravel(), sync.imag.ravel(),"same"))
     corr_index = np.argmax(corr_r)-sync_bits/2*Ns
-    print np.max(np.abs(corr_r))
-    print np.max(np.abs(corr_i))
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(corr_r)
-    plt.plot(corr_i)
-    plt.title("Correlation of signal with known prefix")
+    if plot:
+        print np.max(np.abs(corr_r))
+        print np.max(np.abs(corr_i))
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(corr_r)
+        plt.plot(corr_i)
+        plt.title("Correlation of signal with known prefix")
+
     return corr_index
 
-def decode_symbols(r, i, corr_index, r0, i0, Nbits, fs=48000, baud=300):
+def decode_symbols(r, i, corr_index, r0, i0, Nbits, fs=48000, baud=300,
+        plot=False):
     Ns = fs/baud
     r = r[corr_index:]
     i = i[corr_index:]
@@ -157,22 +172,23 @@ def decode_symbols(r, i, corr_index, r0, i0, Nbits, fs=48000, baud=300):
     r0 = r0[corr_index:]
     i0 = i0[corr_index:]
 
-    print len(r)
-    print len(r0)
-    print len(i0)
+   #print len(r)
+   #print len(r0)
+   #print len(i0)
 
     r = r/np.max(r)*2.2 #change this 2 depending on the input amplitude
     i = i/np.max(i)*2.2 #change this 2 depending on the input amplitude
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(2*r0)
-    plt.plot(r)
-    plt.title('Real part, raw input and normalized')
+    if plot:
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(2*r0)
+        plt.plot(r)
+        plt.title('Real part, raw input and normalized')
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(2*i0)
-    plt.plot(i)
-    plt.title('Imaginary part, raw input and normalized')
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(2*i0)
+        plt.plot(i)
+        plt.title('Imaginary part, raw input and normalized')
 
     ####Decode
     idx = np.r_[Ns/2:len(r):Ns]
@@ -183,24 +199,67 @@ def decode_symbols(r, i, corr_index, r0, i0, Nbits, fs=48000, baud=300):
     r_dec = np.around(r[idx])
     i_dec = np.around(i[idx])
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(2*r0)
-    plt.plot(r)
-    plt.plot(np.around(r))
-    plt.stem(idx, r_dec)
-    plt.title('Real part, decoded by sampling values as indicated')
+    if plot:
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(2*r0)
+        plt.plot(r)
+        plt.plot(np.around(r))
+        plt.stem(idx, r_dec)
+        plt.title('Real part, decoded by sampling values as indicated')
 
-    fig = plt.figure(figsize = (16,4))
-    plt.plot(2*i0)
-    plt.plot(i)
-    plt.plot(np.around(i))
-    plt.stem(idx, i_dec)
-    plt.title('Imaginary part, decoded by sampling values as indicated')
-    fig = plt.figure(figsize = (8,8))
-    plt.scatter(r0*2, i0*2, c='r')
-    plt.scatter(r_dec, i_dec, c='g')
+        fig = plt.figure(figsize = (16,4))
+        plt.plot(2*i0)
+        plt.plot(i)
+        plt.plot(np.around(i))
+        plt.stem(idx, i_dec)
+        plt.title('Imaginary part, decoded by sampling values as indicated')
+        fig = plt.figure(figsize = (8,8))
+        plt.scatter(r0*2, i0*2, c='r')
+        plt.scatter(r_dec, i_dec, c='g')
 
-    plt.title('Constellation of input message vs decoded symbols')
+        plt.title('Constellation of input message vs decoded symbols')
 
     return r_dec + 1j*i_dec
+
+def test_ber(fs=48000):
+    code = np.array((-2-2j,
+        -2-1j,-2+2j,-2+1j,-1-2j,-1-1j,-1+2j,-1+1j,+2-2j,+2-1j,+2+2j,2+1j,1-2j,+1-1j,1+2j,1+1j))/2
+
+    # Compute Error Bit Rate Curves
+    BER = []
+    np.random.seed(seed=1)
+
+    for sigma in np.r_[0.1:8.0:0.2]:
+        Nbits = 100
+        bits = np.int16(random.rand(Nbits,1)*15)
+        rbits = np.int16(random.rand(6,1)*15)
+        prefix = np.array([[0],[2],[10],[8], [0],[2],[10],[8]])
+
+        Nbits = len(rbits) + len(prefix) + len(bits)  # number of bits
+        print "Generating %d random bits"%(Nbits)
+        bits = np.array(rbits.tolist() + prefix.tolist() + bits.tolist())
+
+        # modulate and add noise
+        QAM, M, t, M_prefix, t_prefix = mod_QAM16(bits, prefix, f0=1800, tbw=4,
+                fs=48000, baud=300, shaped=True, plot=False)
+        sig_n = QAM+ sigma*random.randn(len(QAM))
+
+        # demodulate and decode bits with non-coherent and FM demodulators
+        r,i = demod_QAM16(sig_n, t, f0=1800, fs=48000)
+        corr_index = detect_sync(r, i, M_prefix, len(prefix), plot=False)
+        symbols = decode_symbols(r, i, corr_index, M.real.ravel(),
+                M.imag.ravel(), Nbits-len(rbits), plot=False)
+
+        decoded = symbols_to_hex(symbols)
+        errors = np.sum([s[0]!=s[1][0] for s in zip(decoded,
+            bits.tolist()[6:])])
+
+        BER.append(1.0*errors/len(bits.tolist()))
+
+    # plot
+    f = plt.figure()
+    plt.loglog(1/(np.r_[0.1:8.1:0.2]), BER)
+    plt.title("Empirical BER for QAM demodulation")
+    plt.xlabel("SNR")
+    plt.ylabel("BER")
 
