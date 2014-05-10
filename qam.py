c8851909ce0e1a0094cd34c9f67a91fa17de0add
Vikram Iyer <viyers1@gmail.com>
2014-05-09 19:36:51 -0700

    Fixed BER

--- qam.py
+++ qam.py
@@ -3,25 +3,6 @@ from scipy import signal
 import numpy as np
 import pylab as plt
 
-ENCODE = {
-    0: -2-2j,
-    1: -2-1j,
-    2: -2+2j,
-    3: -2+1j,
-    4: -1-2j,
-    5: -1-1j,
-    6: -1+2j,
-    7: -1+1j,
-    8:  2-2j,
-    9:  2-1j,
-    10: 2+2j,
-    11: 2+1j,
-    12: 1-2j,
-    13: 1-1j,
-    14: 1+2j,
-    15: 1+1j,
-}
-
 DECODE = {
     -2:{-2:0, -1:1, 2:2,  1:3},
     -1:{-2:4, -1:5, 2:6,  1:7},
@@ -29,23 +10,23 @@ DECODE = {
      1:{-2:12,-1:13,2:14, 1:15},
 }
 
-#np.array((
-#   -2-2j,
-#   -2-1j,
-#   -2+2j,
-#   -2+1j,
-#   -1-2j,
-#   -1-1j,
-#   -1+2j,
-#   -1+1j,
-#   +2-2j,
-#   +2-1j,
-#   +2+2j,
-#   +2+1j,
-#   +1-2j,
-#   +1-1j,
-#   +1+2j,
-#   +1+1j))
+CODE = np.array((
+        -2-2j,
+        -2-1j,
+        -2+2j,
+        -2+1j,
+        -1-2j,
+        -1-1j,
+        -1+2j,
+        -1+1j,
+        +2-2j,
+        +2-1j,
+        +2+2j,
+        +2+1j,
+        +1-2j,
+        +1-1j,
+        +1+2j,
+        +1+1j))/2.0
 
 #TODO implement these
 def hex_to_symbols():
@@ -75,8 +56,8 @@ def mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=True, plo
     Nbits = len(bits)
     N = Nbits * Ns
 
-    M = np.tile(code[bits],(1,Ns))
-    M_prefix = np.tile(code[prefix],(1,Ns))
+    M = np.tile(CODE[bits],(1,Ns))
+    M_prefix = np.tile(CODE[prefix],(1,Ns))
     t = np.r_[0.0:N]/fs
     t_prefix = np.r_[0.0:len(prefix)*Ns]/fs
     if plot:
@@ -104,18 +85,19 @@ def mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=True, plo
 
     QAM = (M.real.ravel()*np.cos(2*np.pi*f0*t) -
             M.imag.ravel()*np.sin(2*np.pi*f0*t))/2/np.sqrt(2)
-    if shaped:
-        x = np.r_[-2:2:(1.0/147)]
-        h = np.sinc(x)*signal.hann(147*4)
-        impulses = np.zeros(len(bits)*Ns)
-        for i,b in enumerate(bits):
-            impulses[i*Ns]=code[int(b)]
-        QAM = signal.fftconvolve(impulses, h)
-       #print len(QAM)
-        QAM = QAM[(len(QAM)-N)/4:-(len(QAM)-N)*3/4]
-       #print len(t)
-       #print len(QAM)
-        #t1 = r_[0.0:len(QAM)]/fs
+    QAM = QAM.real
+   #if shaped:
+   #    x = np.r_[-2:2:(1.0/147)]
+   #    h = np.sinc(x)*signal.hann(147*4)
+   #    impulses = np.zeros(len(bits)*Ns)
+   #    for i,b in enumerate(bits):
+   #        impulses[i*Ns]=code[int(b)]
+   #    QAM = signal.fftconvolve(impulses, h)
+   #   #print len(QAM)
+   #    QAM = QAM[(len(QAM)-N)/4:-(len(QAM)-N)*3/4]
+   #   #print len(t)
+   #   #print len(QAM)
+   #    #t1 = r_[0.0:len(QAM)]/fs
 
     return QAM, M, t, M_prefix, t_prefix
 
@@ -221,45 +203,43 @@ def decode_symbols(r, i, corr_index, r0, i0, Nbits, fs=48000, baud=300,
 
     return r_dec + 1j*i_dec
 
-def test_ber(fs=48000):
-    code = np.array((-2-2j,
-        -2-1j,-2+2j,-2+1j,-1-2j,-1-1j,-1+2j,-1+1j,+2-2j,+2-1j,+2+2j,2+1j,1-2j,+1-1j,1+2j,1+1j))/2
-
-    # Compute Error Bit Rate Curves
+def test_ber(sigma_range = np.r_[0.01 : 1.478 : 0.1], show_plots=False, verbose=False):
     BER = []
     np.random.seed(seed=1)
 
-    for sigma in np.r_[0.1:8.0:0.2]:
-        Nbits = 100
-        bits = np.int16(random.rand(Nbits,1)*15)
+    for sigma in sigma_range: #np.r_[1.487 : 1.488 : 0.00001]:#np.r_[0.1 : 8.0 : 0.2]:
+        np.random.seed(seed=1)
         rbits = np.int16(random.rand(6,1)*15)
         prefix = np.array([[0],[2],[10],[8], [0],[2],[10],[8]])
+        bits = np.int16(random.rand(26,1)*15)
 
         Nbits = len(rbits) + len(prefix) + len(bits)  # number of bits
-        print "Generating %d random bits"%(Nbits)
+        #print "Generating %d random bits"%(Nbits)
         bits = np.array(rbits.tolist() + prefix.tolist() + bits.tolist())
 
-        # modulate and add noise
-        QAM, M, t, M_prefix, t_prefix = mod_QAM16(bits, prefix, f0=1800, tbw=4,
-                fs=48000, baud=300, shaped=True, plot=False)
-        sig_n = QAM+ sigma*random.randn(len(QAM))
+        QAM, M, t, M_prefix, t_prefix = mod_QAM16(bits, prefix, f0=1800, tbw=4, fs=48000, baud=300, shaped=False, plot=False)
+        #sigma = 0.6
+        QAM += sigma*random.randn(len(QAM))
 
-        # demodulate and decode bits with non-coherent and FM demodulators
-        r,i = demod_QAM16(sig_n, t, f0=1800, fs=48000)
-        corr_index = detect_sync(r, i, M_prefix, len(prefix), plot=False)
-        symbols = decode_symbols(r, i, corr_index, M.real.ravel(),
-                M.imag.ravel(), Nbits-len(rbits), plot=False)
+        i, q = demod_QAM16(QAM, t, f0=1800, fs=48000)
+
+        corr_index = detect_sync(i, q, M_prefix, len(prefix), plot=show_plots)
+
+        symbols = decode_symbols(i, q, corr_index, M.real.ravel(), M.imag.ravel(), Nbits-len(rbits), plot=show_plots)
+
+        #print symbols
+        #print bits.tolist()[6:]
 
         decoded = symbols_to_hex(symbols)
-        errors = np.sum([s[0]!=s[1][0] for s in zip(decoded,
-            bits.tolist()[6:])])
-
-        BER.append(1.0*errors/len(bits.tolist()))
+        error_count = 1.0*np.sum([s[0]!=s[1][0] for s in zip(decoded, bits.tolist()[6:])])
+        if verbose:
+            print 'Error count: %d'%(error_count)
+            print 'sigma=%f errors:%f'%(sigma, error_count/Nbits)
+        BER.append(error_count/Nbits)
 
     # plot
     f = plt.figure()
-    plt.loglog(1/(np.r_[0.1:8.1:0.2]), BER)
-    plt.title("Empirical BER for QAM demodulation")
+    plt.loglog(1/(sigma_range), BER)
+    plt.title("Empirical BER for QAM Demodulation and Correlation")
     plt.xlabel("SNR")
     plt.ylabel("BER")
-
